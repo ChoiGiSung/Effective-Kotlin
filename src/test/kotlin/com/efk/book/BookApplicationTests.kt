6e9d794c18ad6838e4b2ba639cc82d63aa071aae
gisungchoi <chlrltjd159@naver.com>
2022-05-13 00:45:17 +0900

    feat : init

---
+++ src/test/kotlin/com/efk/book/BookApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.efk.book
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BookApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
