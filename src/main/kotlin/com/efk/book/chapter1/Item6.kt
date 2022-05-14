f2be8220753cb29436e34a9f6a4d02699768a3d7
gisungchoi <chlrltjd159@naver.com>
2022-05-15 02:19:56 +0900

    feat : 아이템 6 사용자 정의 오류보다는 표준 오류를 사용하라
    - 적절한 오류가 없을 때는 만들지만 많은 개발자가 알고 있는 표준 오류를 사용하자

---
+++ src/main/kotlin/com/efk/book/chapter1/Item6.kt
@@ -0,0 +1,7 @@
+package com.efk.book.chapter1
+
+import kotlin.reflect.jvm.internal.impl.load.java.structure.JavaClass
+
+//아이템 6 사용자 정의 오류보다는 표준 오류를 사용하라
+//
+//- 적절한 오류가 없을 때는 만들지만 많은 개발자가 알고 있는 표준 오류를 사용하자
