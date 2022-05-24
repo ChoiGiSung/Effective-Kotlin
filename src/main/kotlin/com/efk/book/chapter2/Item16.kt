4863061bdf6049c123c3321e4f36d6a3c20ede36
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-05-24 21:50:56 +0900

    feat : 아이템16 프로퍼티는 동작이 아닌 상태를 나타내야 한다
    - var를 통해 읽고 쓸 수 있는 프로퍼티는 파생 프로퍼티라고 한다
    - 프로퍼티는 본질적으로 함수이다
    - 프로퍼티 getter에 많은 로직을 담지 말라

---
+++ src/main/kotlin/com/efk/book/chapter2/Item16.kt
@@ -0,0 +1,10 @@
+package com.efk.book.chapter2
+
+class Item16 {
+    val sample: String
+        get() = "sample"
+
+
+    // 프로퍼티를 만들지 메소드로 만들지 고민이 된다면
+    // 해당 프로퍼티를 메소드로 만들때 get,set을 붙이지 않을 경우 프로퍼티로 만들면 안된다
+}
