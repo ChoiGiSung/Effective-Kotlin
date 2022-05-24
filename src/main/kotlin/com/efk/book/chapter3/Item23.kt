cc3e3e26d40f73536a11037d2a69a4467bbcfc53
DESKTOP-FL6VBV6\chlrl <chlrltjd159@naver.com>
2022-05-24 22:34:37 +0900

    feat : 아이템23 타입파라미터의 섀동잉을 피하라
    -

---
+++ src/main/kotlin/com/efk/book/chapter3/Item23.kt
@@ -0,0 +1,14 @@
+package com.efk.book.chapter3
+
+class Item23(val name: String) {
+
+    fun sample(name: String) {
+        println(name)
+    }
+}
+
+fun main(){
+    val item : Item23 = Item23("ss")
+
+    item.sample("22")
+}
